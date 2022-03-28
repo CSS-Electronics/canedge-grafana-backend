5d26038f2b8b75dc2227134b6b3d66cebc8d2499
Martin <>
2022-03-28 15:22:56 +0200

    Update

--- canedge_datasource/signal.py
+++ canedge_datasource/signal.py
@@ -204,7 +204,7 @@ def time_series_phy_data(fs, signal_queries: [SignalQuery], start_date: datetime
             print(f"Status before processing: CPU {psutil.cpu_percent()} | RAM used % {psutil.virtual_memory().percent}")
             # Get size of file
             file_size_mb = fs.stat(log_file)["size"] >> 20
-            print("raw file size based on sys: ", sys.getsizeof(log_file) / 1000000)
+            print(f"\nfile_size_mb: {file_size_mb} | data_processed_mb: {data_processed_mb}")
 
             # Check if we have reached the limit of data processed in MB
             if data_processed_mb + file_size_mb > limit_mb:
